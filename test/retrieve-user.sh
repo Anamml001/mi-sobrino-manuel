672a98580b8b0949ac30d1e840d6aaa536ba87cd
Ana María Moya <anamml461@gmail.com>
2024-07-18 18:00:13 +0200

    add changes in test #1

--- test/retrieve-user.sh
+++ test/retrieve-user.sh
@@ -1,1 +1,1 @@
- curl -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2Njk5MWU1YWU2MGYyOTQzMTc0ZjY5NGMiLCJpYXQiOjE3MjEzMTEyOTEsImV4cCI6MTcyMTM5NzY5MX0.XPkApu3SmPG-LRQ034l3xclhU1bGCbhy4nwSiXsHtHs' http://localhost:3000/api/users/66991e5ae60f2943174f694c -v
+ curl -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2Njk5Mjc4YTA5ZTI2MGVkNzNmYjNiZmEiLCJpYXQiOjE3MjEzMTM0ODgsImV4cCI6MTcyMTM5OTg4OH0.uZGO7ShT6k76YmuBAAqR7cRsy-WCbcz7iZHXzLnDYdg' http://localhost:3000/api/users/6699278a09e260ed73fb3bfa -v
