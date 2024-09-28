acf7c4d867f5b01e6fa547b2d5fb72ff08ed8c96
Ana María Moya <anamml461@gmail.com>
2024-09-28 13:18:50 +0200

    create verifyToken function #1

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import errors from '@/app/errors';
 import createPost from '@/app/api/logic/createPost';
 import removePost from '../logic/removePost';
@@ -7,45 +7,55 @@ import retrievePosts from '../logic/retrievePosts';
 import modifyPost from '../logic/modifyPost';
 import { headers } from 'next/headers';
 import toggleLikePost from '../logic/toggleLikePost';
-const { JWT_SECRET } = process.env
 
-const { MatchError, SystemError, ContentError} = errors;
+const JWT_SECRET: string = process.env.JWT_SECRET as string;
+
+
+const { MatchError, SystemError, ContentError } = errors;
+
+function verifyToken(req: NextRequest) {
+    const authorization: string | null = headers().get('authorization')
+
+    const token: any = authorization?.slice(7)
+
+    const payload: JwtPayload = jwt.verify(token, JWT_SECRET) as { sub: string }
+
+    return payload
+}
+
 //createPost
 export async function POST(req: NextRequest) {
-     
-            const { title, image, video, text }:{ title: string, image?: string , video?: string, text: string} = await req.json();
+    try {
+        const { title, image, video
+            , text }: { title: string, image?: string, video?: string, text: string } = await req.json();
 
-            const authorization = headers().get('authorization')
-            
-            const token = authorization?.slice(7)
+        const { sub: userId } = verifyToken(req)
 
-            const { sub: userId }:{sub:string} = jwt.verify(token, JWT_SECRET)
-            try {
-                await createPost(userId, title, image, video, text );
-        
-                return new NextResponse(null, { status: 201 });
-            } catch (error) {
-                const _error = (error as Error);
-        
-                if (_error instanceof MatchError) {
-                    return NextResponse.json({ error: _error.constructor.name, message: _error.message }, { status: 400 });
-                } else if (_error instanceof SystemError) {
-                    return NextResponse.json({ error: _error.constructor.name, message: _error.message }, { status: 500 });
-                }
-        
-                return NextResponse.json({ error: ContentError.name, message: _error.message }, { status: 500 });
-            }
+        await createPost(userId!, title, text, image, video);
+
+        return new NextResponse(null, { status: 201 });
+    } catch (error) {
+        const _error = (error as Error);
+
+        if (_error instanceof MatchError) {
+            return NextResponse.json({ error: _error.constructor.name, message: _error.message }, { status: 400 });
+        } else if (_error instanceof SystemError) {
+            return NextResponse.json({ error: _error.constructor.name, message: _error.message }, { status: 500 });
+        }
+
+        return NextResponse.json({ error: ContentError.name, message: _error.message }, { status: 500 });
+    }
 }
 //removePost
 export async function DELETE(req: NextRequest) {
-      
-    const { postId }: {postId:string} = await req.json();
+
+    const { postId }: { postId: string } = await req.json();
 
     const authorization = headers().get('authorization')
-    
+
     const token = authorization?.slice(7)
 
-    const { sub: userId }:{sub:string} = jwt.verify(token, JWT_SECRET)
+    const { sub: userId }: { sub: string } = jwt.verify(token, JWT_SECRET)
     try {
         await removePost(userId, postId);
 
@@ -73,7 +83,7 @@ export async function GET(req: NextRequest) {
     }
 
     try {
-        const { sub: userId }:{ sub: string } = jwt.verify(token, JWT_SECRET);
+        const { sub: userId }: { sub: string } = jwt.verify(token, JWT_SECRET);
 
         const posts = await retrievePosts(userId);
 
@@ -94,17 +104,17 @@ export async function GET(req: NextRequest) {
 }
 //modifyPost
 export async function PATCH(req: NextRequest) {
-        
-    const { postId, text}:{postId:string, text:string} = await req.json();
+
+    const { postId, text }: { postId: string, text: string } = await req.json();
 
     const authorization = headers().get('authorization')
-    
+
     const token = authorization?.slice(7)
 
-    const { sub: userId }:{sub: string} = jwt.verify(token, JWT_SECRET)
+    const { sub: userId }: { sub: string } = jwt.verify(token, JWT_SECRET)
 
     try {
-        await modifyPost(userId, postId, text );
+        await modifyPost(userId, postId, text);
 
         return new NextResponse(null, { status: 204 });
     } catch (error) {
@@ -121,14 +131,14 @@ export async function PATCH(req: NextRequest) {
 }
 //toggleLikePost
 export async function PUT(req: NextRequest) {
-        
-    const { postId}:{postId:string} = await req.json();
+
+    const { postId }: { postId: string } = await req.json();
 
     const authorization = headers().get('authorization')
-    
+
     const token = authorization?.slice(7)
 
-    const { sub: userId }:{sub: string} = jwt.verify(token, JWT_SECRET)
+    const { sub: userId }: { sub: string } = jwt.verify(token, JWT_SECRET)
 
     try {
         await toggleLikePost(userId, postId);
