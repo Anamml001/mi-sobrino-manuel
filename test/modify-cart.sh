c14c44ae2be2bc8f0323ea578b67fe8074a8f3c1
Ana María Moya <anamml461@gmail.com>
2024-10-22 17:45:23 +0200

    implement removeCart.ts and modifyCart.ts with test in api #1

---
+++ test/modify-cart.sh
@@ -0,0 +1,3 @@
+curl -X PATCH -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjlhN2E0MDAwMTVhNDAzMmM3YWJiZGUiLCJpYXQiOjE3Mjk1OTc5NDksImV4cCI6MTcyOTY4NDM0OX0.431JrMCUK7lxISLEiz5k64LBFQgbXRG__KSDJY5kJFM" -H "Content-Type: application/json" -d '{"cartId": "6717c1a82ca0e7ad63989114", "itemInput": {"id": "67139c4262ca7d485c83c364", "quantity": 2}}' http://localhost:3000/api/cart -v
+
+
---
+++ test/remove-cart.sh
@@ -0,0 +1,1 @@
+curl -X DELETE -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjlhN2E0MDAwMTVhNDAzMmM3YWJiZGUiLCJpYXQiOjE3Mjk1OTc5NDksImV4cCI6MTcyOTY4NDM0OX0.431JrMCUK7lxISLEiz5k64LBFQgbXRG__KSDJY5kJFM' -H 'Content-type: application/json' -d '{"cartId": "6717b1d62ca0e7ad639890fe"}' http://localhost:3000/api/cart -v
